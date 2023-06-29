1996c05f04f59d5cae274b3bc7af8206a9bb556c
Lucas Melo de Carvalho <lucas_m@id.uff.br>
2023-06-28 22:13:08 -0300

    testes

--- teste.py
+++ teste.py
@@ -9,18 +9,10 @@ Trabalho 2
 
 #mostra o conteudo do csv
 import pandas as pd
-import matplotlib.pyplot as mp
+#import matplotlib.pyplot as mp
 
 #mostra o conteudo do csv
-df = pd.read_csv('epl_results_2022-23.csv')
-print(df.to_string())
-
-df.plot.scatter(x = 'HY',y = 'HF')
-mp.show()
-
-"""
-for d in df:
-    print(d[15]); print(d[17])
-    #mp.plot(d[15],d[17])
-    #mp.show()
-"""
+resultados = pd.read_csv('epl_results_2022-23.csv')
+lista_colunas = list(resultados.columns)
+print("Colunas : ",lista_colunas)
+#print(resultados.to_string())
